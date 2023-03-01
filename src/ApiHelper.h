5437e71538c2cb0aea7f31c652ddd92ade8af054
Stanislav Gadzhov <66910851+svgmaster@users.noreply.github.com>
2023-03-01 19:00:03 +0100

    Change ApiHelper to class with async support...
    Change ApiHelper to class with asynchronously support

--- src/ApiHelper.h
+++ src/ApiHelper.h
@@ -1,36 +1,37 @@
 /**************************************************************************************\
-*								         	       *
-*   	MIT License								       *
-*										       *
-*   	Community-maintained OpenAI API Library for modern C++			       *
-*										       *
-*   	Copyright (c) 2023 Stanislav Gadzhov					       *
-*								      		       *
-*   	Permission is hereby granted, free of charge, to any person obtaining a copy   *
-*	of this software and associated documentation files (the "Software"), to deal  *
-*	in the Software without restriction, including without limitation the rights   *
-*	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
-*	copies of the Software, and to permit persons to whom the Software is	       *
-*	furnished to do so, subject to the following conditions:		       *
-*										       *
-*	The above copyright notice and this permission notice shall be included in all *
-*	copies or substantial portions of the Software.				       *
-*								 		       *
-*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
-*	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
-*	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
-*	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	       *
-*	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
-*	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
-*	SOFTWARE.								       *
-*										       *
+*								         	       
+*   MIT License								       
+*										       
+*   Community-maintained OpenAI API Library for modern C++			       
+*										      
+*   Copyright (c) 2023 Stanislav Gadzhov					       
+*								      		       
+*   Permission is hereby granted, free of charge, to any person obtaining a copy   
+*	of this software and associated documentation files (the "Software"), to deal  
+*	in the Software without restriction, including without limitation the rights   
+*	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      
+*	copies of the Software, and to permit persons to whom the Software is	       
+*	furnished to do so, subject to the following conditions:		       
+*										       
+*	The above copyright notice and this permission notice shall be included in all 
+*	copies or substantial portions of the Software.				       
+*								 		       
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     
+*	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       
+*	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    
+*	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	       
+*	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  
+*	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  
+*	SOFTWARE.								       
+*										       
 \**************************************************************************************/
 
 #ifndef OPENAIAPI_API_HELPER_H
-#define OPENAIAPI_API_HELPER_H 1
+#define OPENAIAPI_API_HELPER_H
 
 #include <map> /* std::map */
 #include <string> /* std::string */
+#include <future> /* std::future */
 
 /* Define when libcurl is build with static library */
 #define CURL_STATICLIB
@@ -39,22 +40,46 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
+#include <OpenAIApi/Response.h>
+using namespace OpenAIApi;
+
 /* write callback */
 size_t _write_buf(char* ptr, size_t size, size_t nmemb, void* buffer);
 
-/* this function doing all magic */
-int sendRequest(std::string& url, std::string& type, const char* payload, size_t length, 
-	std::string& api_key, std::string& org_key, std::string content_type = "Content-Type: application/json",
-	bool post_form = false);
+class ApiHelper
+{
 
-/* add form */
-void addFormParam(std::string key, std::string value);
+public:
 
-/* convert response string to json object */
-json stringToJson(std::string response);
+	ApiHelper( std::string apiKey, std::string organizationKey = "" );
 
-extern std::map<std::string, std::string> m_formMap;
-extern std::string m_responseBody;
-extern int m_httpCode;
+	~ApiHelper() = default;
+
+	/* this function doing all magic */
+	int request(std::string& url, std::string& type, std::string content_type = "Content-Type: application/json");
+
+	/* add form */
+	void addFormParam(std::string key, std::string value);
+
+	/* convert response string to json object */
+	json stringToJson(std::string response);
+
+	void setJsonRequest(json jsonRequest);
+
+	//template<class T>
+	std::future< Response < json >> getResponse();
+
+protected:
+
+	int m_httpCode;
+	bool is_JsonRequest;
+	bool is_formRequest; 
+	std::string m_orgId;
+	std::string m_apiKey;
+	std::string m_payload;
+	std::string m_responseBody;
+	std::map<std::string, std::string> m_formMap;
+
+};
 
 #endif /* !OPENAIAPI_API_HELPER_H */
