766dfeff1e8280e318e8437c9d2c4d256141db97
Stanislav Gadzhov <66910851+svgmaster@users.noreply.github.com>
2023-03-01 18:58:03 +0100

    Add std::future return type for asynchronously

--- src/OpenAIApi.cpp
+++ src/OpenAIApi.cpp
@@ -27,333 +27,271 @@
 \**************************************************************************************/
 
 #include <string> /* we use std::strings */
-#include <sstream> /* this is need for std::stringstream */
+#include <future> /* this is need for std::future */
 
 #include <OpenAIApi/Client.h> /* define all functions, and use nlohmann/json */
 
 #include "EndPoints.h" /* getOpenAIUrl */
-#include "ApiHelper.h" /* sendRequest, stringToJson */
+#include "ApiHelper.h" /* request, getResponse */
 
 using namespace OpenAIApi;
 
-json
+ApiHelper* api = nullptr;
+
+Client::Client(std::string apiKey, std::string organizationKey)
+{
+	api = new ApiHelper(apiKey, organizationKey);
+	m_apikey = apiKey, m_orgkey = organizationKey;
+};
+
+
+Client::~Client()
+{
+	delete api;
+}
+
+std::future< Response< json >>
 Client::cancelFineTune(std::string fineTuneId)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::CancelFineTune, fineTuneId);
-
-	std::string payload = "";
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createCompletion(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Completions);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createEdit(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Edit);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createEmbedding(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Embeddings);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createFile(std::string file, std::string purpose)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::UploadFiles);
-
-	addFormParam("file", file);
-	addFormParam("purpose", purpose);
-
-	std::string payload = "";
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey, "Content-Type : application/x-www-form-urlencoded", true);
+	api->addFormParam("file", file);
+	api->addFormParam("purpose", purpose);
+	api->request(url, type, "Content-Type : application/x-www-form-urlencoded");
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createFineTune(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::FineTunes);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createImage(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ImagesGenerations);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createImageEdit(std::string image, std::string prompt, std::string mask, int n,
 	std::string size, std::string responseFormat, std::string user)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ImagesEdits);
-
-	addFormParam("image", image);
-	addFormParam("mask", mask);
-	addFormParam("prompt", prompt);
-	addFormParam("n", std::to_string(n));
-	addFormParam("size", size);
-	addFormParam("response_format", responseFormat);
-	addFormParam("user", user);
-
-	std::string payload = "";
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey, "Content-Type: application/x-www-form-urlencoded", true);
+	api->addFormParam("image", image);
+	api->addFormParam("mask", mask);
+	api->addFormParam("prompt", prompt);
+	api->addFormParam("n", std::to_string(n));
+	api->addFormParam("size", size);
+	api->addFormParam("response_format", responseFormat);
+	api->addFormParam("user", user);
 
-	return stringToJson(m_responseBody);
+	api->request(url, type, "Content-Type: application/x-www-form-urlencoded");
+
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createImageVariation(std::string image, int n, std::string size, std::string responseFormat, 
 	std::string user)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ImagesVariations);
-
-	addFormParam("image", image);
-	addFormParam("n", std::to_string(n));
-	addFormParam("size", size);
-	addFormParam("response_format", responseFormat);
-	addFormParam("user", user);
-
-	std::string payload = "";
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()),
-		payload.length(), m_apikey, m_orgkey, "Content-Type: application/x-www-form-urlencoded", true);
+	api->addFormParam("image", image);
+	api->addFormParam("n", std::to_string(n));
+	api->addFormParam("size", size);
+	api->addFormParam("response_format", responseFormat);
+	api->addFormParam("user", user);
 
-	return stringToJson(m_responseBody);
+	api->request(url, type, "Content-Type: application/x-www-form-urlencoded");
+
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::createModeration(json jsonRequest)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Moderation);
-
-	std::stringstream ss;
-	ss << jsonRequest;
-
-	std::string payload = ss.str();
 	std::string type = "POST";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->setJsonRequest(jsonRequest);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::deleteFile( std::string fileId )
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::DeleteFiles, fileId);
-
-	std::string payload = "";
 	std::string type = "DELETE";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()),
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::deleteModel( std::string model )
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::DeleteFineTuneModel, model);
-
-	std::string payload = "";
 	std::string type = "DELETE";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()),
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::downloadFile( std::string fileId )
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::RetrieveFileContent, fileId);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::listFiles()
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ListFiles);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()),
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::listFineTuneEvents(std::string fineTuneId, bool stream)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ListFineTuneEvents, fineTuneId);
 
-	addFormParam("stream", std::to_string(stream));
-
-	std::string payload = "";
+	api->addFormParam("stream", std::to_string(stream));
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey,"Content - Type: application / json", true);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::listFineTunes()
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::ListFineTunes);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::listModels()
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Models);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::retrieveFile(std::string fileId)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::RetrieveFile, fileId);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::retrieveFineTune(std::string fineTuneId)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::RetrieveFineTune, fineTuneId);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()), 
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
 
-json
+std::future< Response< json >>
 Client::retrieveModel(std::string model)
 {
 	std::string url = getOpenAIUrl(OpenAIEndpoint::Model, model);
-
-	std::string payload = "";
 	std::string type = "GET";
 
-	m_httpCode = sendRequest(url, type, reinterpret_cast<const char*>(payload.c_str()),
-		payload.length(), m_apikey, m_orgkey);
+	api->request(url, type);
 
-	return stringToJson(m_responseBody);
+	return api->getResponse();
 }
-
-std::string Client::getResponseBody() { return m_responseBody; }
-
-int Client::getResponseCode() { return m_httpCode; }
